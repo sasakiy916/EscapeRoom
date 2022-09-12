fb6a1b7fc6115e7b16d17d31c5b35b7d7c7f83b6
sasakiy916 <sasaki3y98521j@gmail.com>
2022-09-12 23:29:38 +0900

    タイトル サウンドマネージャー機能(仮) 実装

---
+++ Assets/Sasaki/Scripts/Title/SoundManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //BGM,SEのオーディオソース
+    [SerializeField] AudioSource bgmAudioSource;
+    [SerializeField] AudioSource seAudioSource;
+
+    //各種AudioClip登録用リスト
+    [SerializeField] List<BGMSoundData> bgmSoundDatas;
+    [SerializeField] List<SESoundData> seSoundDatas;
+
+    //各種オーディオボリューム
+    public float masterVolume = 1;
+    public float bgmMasterVolume = 1;
+    public float seMasterVolume = 1;
+
+    //シングルトン化
+    public static SoundManager instance { get; private set; }
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //既に別のサウンドマネージャーが存在する場合は消す
+            Destroy(gameObject);
+        }
+    }
+
+    //音源再生 関数
+    //BGM
+    public void PlayBGM(BGMSoundData.BGM bgm)
+    {
+        BGMSoundData data = bgmSoundDatas.Find(data => data.bgm == bgm);//リストから指定BGMを取得
+        bgmAudioSource.clip = data.audioClip;//AudioSourceにAudioClip設定
+        bgmAudioSource.volume = data.volume * bgmMasterVolume * masterVolume;//音量設定
+        bgmAudioSource.Play();//BGM再生
+    }
+    //SE
+    public void PlaySE(SESoundData.SE se)
+    {
+        SESoundData data = seSoundDatas.Find(data => data.se == se);//リストから指定SEを取得
+        seAudioSource.volume = data.volume * seMasterVolume * masterVolume;//音量設定
+        seAudioSource.PlayOneShot(data.audioClip);//SEを一回再生
+    }
+}
+
+//BGMデータのクラス
+[System.Serializable]
+public class BGMSoundData
+{
+    public enum BGM
+    {
+        TITLE,
+    }
+    public BGM bgm;
+    public AudioClip audioClip;
+    [Range(0, 1)] public float volume = 1.0f;
+}
+
+//SEデータのクラス
+[System.Serializable]
+public class SESoundData
+{
+    public enum SE
+    {
+        CORRECT,
+        ClickStart,
+    }
+    public SE se;
+    public AudioClip audioClip;
+    [Range(0, 1)] public float volume = 1.0f;
+}
--- Assets/Sasaki/Scripts/Title/TitleManager.cs
+++ Assets/Sasaki/Scripts/Title/TitleManager.cs
@@ -5,22 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    public string loadScene;
-    AudioSource audioSource;
-    [SerializeField]AudioClip clickSE;
+    public string loadScene;//遷移先シーンの名前
+
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        //BGM再生
+        SoundManager.instance.PlayBGM(BGMSoundData.BGM.TITLE);
     }
     void Update()
     {
+        //デバッグ用の処理
+        if (Input.GetKeyDown("a")) SoundManager.instance.PlaySE(SESoundData.SE.ClickStart);
+        if (Input.GetKeyDown("b")) SoundManager.instance.PlaySE(SESoundData.SE.CORRECT);
     }
-    public void MoveScene(){
+
+    //シーン遷移 関数
+    public void MoveScene()
+    {
         StartCoroutine(SECoroutine());
     }
-    IEnumerator SECoroutine(){
-        audioSource.PlayOneShot(clickSE);
+
+    //画面クリックで音鳴らした後シーン遷移 コルーチン
+    IEnumerator SECoroutine()
+    {
+        SoundManager.instance.PlaySE(SESoundData.SE.ClickStart);//SE再生
+        // audioSource.PlayOneShot(clickSE);
         yield return new WaitForSeconds(1.0f);
-        SceneManager.LoadScene(loadScene);
+        SceneManager.LoadScene(loadScene);//シーン遷移
     }
 }
