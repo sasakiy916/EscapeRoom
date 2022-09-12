9be1a748a638f8bf27b3051214e261ccd24d0797
cshm225 <cshm225@gmail.com>
2022-09-12 12:22:11 +0900

    バグ修正

--- Assets/Matsuoka/Assets/Scripts/Escape.cs
+++ Assets/Matsuoka/Assets/Scripts/Escape.cs
@@ -8,7 +8,7 @@ public class Escape : MonoBehaviour
     [SerializeField] ClearKeys clearKeys;
     public void OnclickEscape()
     {
-        clearKeys.UpdateKeys(SceneManager.GetActiveScene().name);
+        PlayerPrefs.SetInt("Matsuoka", 1);
         Debug.Log("Main" + PlayerPrefs.GetInt("Matsuoka"));
         SceneManager.LoadScene("CentralRoom");
     }
