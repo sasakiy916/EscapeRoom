5a02a62f1773d9f263e8ab8f00945a565491f622
sasakiy916 <sasaki3y98521j@gmail.com>
2022-09-15 00:36:29 +0900

    スマホ操作,画面 仮対応

--- Assets/Matsuoka/Assets/Scripts/MoveCamera.cs
+++ Assets/Matsuoka/Assets/Scripts/MoveCamera.cs
@@ -44,7 +44,26 @@ public class MoveCamera : MonoBehaviour
                 cameras[0].transform.rotation = defaultRotation;
             }
         }
-
+    }
+    //スマホでカメラ操作するためボタン
+    public void MoveCameraBottun()
+    {
+        //cameras[cameraNumber].SetActive(false);
+        cameraNumber++;
+        if (cameraNumber >= cameras.Length)
+        {
+            cameraNumber = 0;
+        }
+        if (cameraNumber != 0)
+        {
+            cameras[0].transform.position = cameras[cameraNumber].transform.position;
+            cameras[0].transform.rotation = cameras[cameraNumber].transform.rotation;
+        }
+        else
+        {
+            cameras[0].transform.position = defaultPosition;
+            cameras[0].transform.rotation = defaultRotation;
+        }
     }
 
     public void OnClickMoveCamera(Vector3 cameraPosition, Quaternion cameraRotation)
